8bc67d12b3e2d11bb00573b0878abe9d82817be7
deepshikhachadokar <deepshikhachadokar09@gmail.com>
2024-12-17 22:12:40 +0530

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from '../container/header/footer/footer.component';
+import { HeaderComponent } from '../container/header/header/header.component';
+import { SidenavComponent } from '../container/sidenav/sidenav/sidenav.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone:true,
+  imports: [FooterComponent, HeaderComponent,SidenavComponent, FooterComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'nglearning';
